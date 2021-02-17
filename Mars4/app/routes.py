edab868a7746b6ce1f8ee5beaf4048c50ca7ac68
bjarmstr <78768368+bjarmstr@users.noreply.github.com>
2021-02-16 20:20:15 -0700

    update a value from redis using javascript

--- Mars4/app/routes.py
+++ Mars4/app/routes.py
@@ -4,12 +4,9 @@ Created on Jan. 29, 2021
 @author: Jean Armstrong
 '''
 from app import app
-from flask import render_template, request, flash
+from flask import render_template, request, flash, Response
 from app import redis_client
 from app.computations import scenario_list, isInteger
-
-
-
 
 
 #context process allows variable to be passed to all routes
@@ -21,10 +18,19 @@ def context_processor():
 def redisbox():
     myvar = redis_client.get('potato')
     print(myvar)
-    myvar2 = scenario_list('test')
-    print (myvar2, "myvar2")
-    return render_template('redisbox.html', title='Redisbox')
+    #myvar2 = scenario_list('test')
+    #print (myvar2, "myvar2")
+    return render_template('redisbox.html', title='Redisbox', myvar=myvar)
 
+@app.route('/stream')
+def stream():  
+    def event_stream():
+        while True:
+            new_stream_data = redis_client.xread({'chat': '$', 'chat2':'$'}, block=0, count=10)
+            yield 'data: %s\n\n' % new_stream_data   
+    return Response(event_stream(),
+                          mimetype="text/event-stream")
+ 
 @app.route('/')
 @app.route('/scenario')
 @app.route('/index', methods=['GET', 'POST'])
@@ -34,6 +40,7 @@ def scenario():
         checkOn = request.form 
         base_time = request.form['base-time']  
         if not isInteger(base_time):
+            flash('CORRECT Entry Error and RESUBMIT')
             error = " Base time must be an integer "
             clock = 30
         else:
@@ -43,7 +50,7 @@ def scenario():
         checkOn = ""
         base_time = 30
         clock = 30
-    scenario_list = [{'Fuse Blows': '5', 'Control board fails': '10', 'CO2 line leak': '15', 'D': '12'}]
+    scenario_list = {'Fuse Blows': '5', 'Control board fails': '10', 'CO2 line leak': '15', 'D': '12'}
     return render_template('scenario.html', title='Scenarios', scenario_list=scenario_list, checkOn = checkOn, base_time = base_time, clock=clock, error=error)
 
 
@@ -53,15 +60,23 @@ def piping():
 
 @app.route('/operations', methods=['GET', 'POST'])
 def operations():
+    error = None
     if request.method == 'POST':
-        SRA_override= request.form['SRA_rate']
-        CDRA_override=request.form['CDRA_rate']
-        f = request.form
-        for key in f.keys():
-            for value in f.getlist(key):
+        #SRA_override= request.form['SRA_rate']
+        op_data = request.form
+        for key, value in op_data.items():
+                if value: #was a value entered in the field?
+                    if not isInteger(value):
+                        flash('FIX Entry Error and RESUBMIT')
+                        error = "All values must be an integer "
+                    else:
+                        to_database= key
                 print (key,":",value)
+      
     else:
-        SRA_override = "number?"
-        CDRA_override = "need initial value?"
+        op_data = ""  #need initial value from database
+
         
-    return render_template('operations.html', title='Operations' , SRA_override=SRA_override, CDRA_override=CDRA_override)
+    return render_template('operations.html', title='Operations',
+                           error=error, op_data=op_data)
+
