ce97ab85b66a8a77c04a9f788dc21f60197c9591
Jean Armstrong <bjarmstr@yahoo.com>
2021-04-16 15:11:08 -0600

    fully running off redis instead of dictionary

--- Mars5/marscontrol.py
+++ Mars5/marscontrol.py
@@ -14,9 +14,12 @@ MIN_TEMP = 25
 PREHEAT_TEMP = 130
 WPA_HOLD_TIME = 10 #20seconds
 DEG_INC = 5 #5 degrees per increment
-TIME_INC = .3  #1 second delay between increments
+TIME_INC = .1  #1 second delay between increments
 #(PREHEAT_TEMP-MIN_TEMP)/DEG_INC*TIME_INC = Wait time of preheat cycle
-SRA_OGA_SYNC_TIME = 4
+SRA_OGA_SYNC_TIME = 4 #seconds
+
+
+sync_start = 0
 
 hold_start_time = 0
 temp_cycle = "off"
@@ -25,12 +28,13 @@ preheating = True
 
 user_str = ""
 next_val = False
-assemblies = {"OGA":{"rate":0,"start_time":time(), "error_code":0}, 
-              "SRA":{"rate":0,"start_time":time(),"error_code":0, "N2":False}, 
-              "WPA":{"rate":0,"start_time":time(), "error_code":0, "temp":MIN_TEMP}, 
-              "CDRA":{"rate":0,"start_time":time(), "error_code":0}}
+
 
 temp_cycle = "off"   #values: off, preheat or cooling
+
+
+assemblies = {"CDRA","SRA","OGA","WPA"}
+
 
 def main():
     wpa_temp_status_thread = threading.Thread(target=wpa_temp_status, daemon=True)
@@ -42,8 +46,12 @@ def main():
     xadd_redis("OGA","error",0)
     xadd_redis("WPA","error",0)
     xadd_redis("CDRA","error",0)
-    xadd_redis("SRA-purge","purge",False)
-    xadd_redis("WPA-preheat","preheat",MIN_TEMP)
+    xadd_redis("SRA","purge",0)
+    xadd_redis("WPA","preheat",MIN_TEMP)
+    xadd_redis("SRA","rate",0)
+    xadd_redis("OGA","rate",0)
+    xadd_redis("WPA","rate",0)
+    xadd_redis("CDRA","rate",0)
     startup()
 
 
@@ -73,35 +81,37 @@ def startup():
 
 
 def set_status():
-    global user_str,next_val, temp_cycle
+    global user_str,next_val, temp_cycle, sync_start
     if (next_val == True):
         for assembly in assemblies:
-            if assembly in user_str:
-                if (assemblies[assembly]["rate"] == 0):
-                    
-                    #assemblies[assembly]["error_code"] = pre_conditions(assembly) 
+            if assembly in user_str: #if we try to start this box
+                if (get_redis(assembly) == 0):  #make sure it is not already started
                     err_code = pre_conditions(assembly)
                     xadd_redis(assembly,"error", err_code)
                     if (assembly =="WPA"):  #WPA needs a preheat before starting
                         if temp_cycle == "off":
                             temp_cycle = "start_preheat"
                         elif temp_cycle == "cooling":
-                            temp_cycle = "start_preheat"
-                    #if (assemblies[assembly]["error_code"] == 0):  
-                    if (err_code == 0): 
-                        assemblies[assembly]["start_time"] = time()
-                        assemblies[assembly]["rate"] = 100  
+                            temp_cycle = "start_preheat" 
+                    if (err_code == 0):
+                        #start-up unit
+                        if assembly == "SRA":
+                            SRA_start = time()
+                            sync_start = time()
+                            print("sync_start SRA", sync_start)
+                        if assembly == "OGA":
+                            OGA_start = time()  
+                            sync_start = time()
+                            print("start timer - OGA")
+                        xadd_redis(assembly,"rate", 100)
                     
         if "N2" in user_str:  #SRA needs N2 purge before starting
-            #if (assemblies["SRA"]["error_code"] == 308):
-                #assemblies["SRA"]["error_code"] = 0
             err_code = get_redis("SRA-error")
             if err_code == 308:
                 xadd_redis("SRA","error", 0)
             print(get_redis("SRA-purge"))
-            if (assemblies["SRA"]["rate"] == 0):
-                xadd_redis("SRA","purge", True)
-                #assemblies["SRA"]["N2"]= True
+            if get_redis("SRA") == 0:
+                xadd_redis("SRA","purge", 1)
             else: print("SRA lines can't be purged while it is running")
 
             
@@ -111,17 +121,17 @@ def print_status():
     if (next_val == True):
         completed = True
         for assembly in assemblies:
-            print(assembly, "rate", assemblies[assembly]["rate"],"%",assemblies[assembly]["error_code"],get_redis(assembly+"-error"),"Error")
-            if assemblies[assembly]["rate"] == 0:
+            print(assembly, "rate", get_redis(assembly),"%",get_redis(assembly+"-error"),"Error")
+            if get_redis(assembly) == 0:
                 completed = False
         if completed == True:
             print("CONGRATULATIONS!  You are now making oxygen")
         print("N2",get_redis("SRA-purge"))  #light led to indicate this
         if (get_redis("WPA-error") == 101):
             print("WPA Preheating Temp Required: 130 C, Actual: ", get_redis("WPA-preheat"), "C")
-        if (assemblies["WPA"]["rate"] == 0) and (temp_cycle == "holding") :
+        if (get_redis("WPA") == 0) and (temp_cycle == "holding") :
             print("WPA preheated, ready for start-up")
-        if (assemblies["SRA"]["error_code"] == 308):
+        if (get_redis("SRA-error") == 308):
             print("N2 purge required before SRA start-up")
         if (get_redis("SRA-error") == 534) or (get_redis("OGA-error") == 543):
             print("OGA/SRA must be started within ",SRA_OGA_SYNC_TIME," seconds of each other" )
@@ -130,12 +140,10 @@ def print_status():
             xadd_redis("SRA","error",0)
         if get_redis("SRA-error") == 313:
             print("No CO2 available for startup")
-            #assemblies["SRA"]["error_code"]  = 0
             xadd_redis("SRA","error",0)
             
         if get_redis("OGA-error") == 414:
             print("No potable water available for startup")
-            #assemblies["OGA"]["error_code"]  = 0
             xadd_redis("OGA","error",0)
         
     next_val = False
@@ -143,38 +151,35 @@ def print_status():
 
 def pre_conditions(assembly):
     global temp_cycle
-    if (assembly == "WPA") and (assemblies["WPA"]["rate"] == 0):
+    if (assembly == "WPA") and (get_redis("WPA") == 0):
         if (temp_cycle != "holding"):
             xadd_redis(assembly,"error", 101)
-            #assemblies[assembly]["error_code"]  = 101
         else: xadd_redis(assembly,"error", 0)
-            #assemblies[assembly]["error_code"]  = 0
-    elif (assembly == "SRA") and (assemblies["SRA"]["rate"] == 0):  
-        if (get_redis("SRA-purge")==False):
-            #assemblies[assembly]["error_code"]  = 308 
+    elif (assembly == "SRA") and (get_redis("SRA") == 0):  
+        if (get_redis("SRA-purge")== 0):
             xadd_redis(assembly,"error", 308)  
-        elif (assemblies["CDRA"]["rate"] == 0):
-            #assemblies["SRA"]["error_code"]  = 313
+        elif (get_redis("CDRA") == 0):
+            ##build for level changes
             xadd_redis(assembly,"error", 313)
-    elif (assembly == "OGA") and (assemblies["WPA"]["rate"] == 0):
-        #assemblies[assembly]["error_code"]  = 414
+    elif (assembly == "OGA") and (get_redis("WPA") == 0):
+        ##build for level changes
         xadd_redis(assembly,"error", 414)
-    else: xadd_redis(assembly,"error", 0)
-        #assemblies[assembly]["error_code"]  = 0  #no error codes - allow startup          
+    else: xadd_redis(assembly,"error", 0)           
     err_code = get_redis(assembly+"-error")
     return err_code   
 
 def sra_oga_dependency():
-    if (assemblies["SRA"]["rate"]>0) and (assemblies["OGA"]["rate"]==0):
-        if (time()-assemblies["SRA"]["start_time"]) > SRA_OGA_SYNC_TIME:
-            assemblies["SRA"]["rate"]= 0
-            get_redis("SRA-purge")=False
-            #assemblies["SRA"]["error_code"] = 534
+    global sync_start
+    if (get_redis("SRA")>0) and (get_redis("OGA")==0):
+        if (time()-sync_start) > SRA_OGA_SYNC_TIME:
+            
+            print("sra/oga", time(), sync_start, time()-sync_start)
+            xadd_redis("SRA","rate", 0)
+            xadd_redis("SRA","purge", 0)
             xadd_redis("SRA","error", 534)
-    if (assemblies["OGA"]["rate"]>0) and (assemblies["SRA"]["rate"]==0):
-        if (time()-assemblies["OGA"]["start_time"]) > SRA_OGA_SYNC_TIME:
-            assemblies["OGA"]["rate"]= 0
-            #assemblies["OGA"]["error_code"] = 543
+    if (get_redis("OGA")>0) and (get_redis("SRA")==0):
+        if (time()-sync_start) > SRA_OGA_SYNC_TIME:
+            xadd_redis("OGA","rate", 0) 
             xadd_redis("OGA","error", 543)
 
 def wpa_temp_status():
@@ -185,7 +190,6 @@ def wpa_temp_status():
             wpa_preheat()   
         elif temp_cycle == "start_holding":
             hold_start_time = time()
-            #assemblies["WPA"]["error_code"]=0
             xadd_redis("WPA","error", 0)
             temp_cycle = "holding"
         elif temp_cycle == "holding":
@@ -216,7 +220,6 @@ def wpa_cooling():
     while temp_cycle== "cooling":  
         temperature -= DEG_INC
         xadd_redis("WPA","preheat", temperature)
-        #assemblies["WPA"]["temp"] -= DEG_INC
         if (temperature <= MIN_TEMP):
             temp_cycle = "off"
         sleep(1)   
@@ -228,17 +231,13 @@ def xadd_redis(assembly,stream_type,value):
         r.xadd((assembly),{stream:value})
     else:
         r.xadd((stream),{stream:value})
-    #r.xadd(("WPA"),{"WPA-rate":"0"})
 
 def get_redis(equip_stream):
-    #Finds the newest rate/level from the stream and the time it was recorded
-    #Redis stream id's starts with time from Unix Epoch
-    #value is stored as the value in a dictionary datatype
+    #Finds the newest rate/level from the stream
     raw_data = r.xrevrange(equip_stream,"+","-",1)
     dict_info = ((raw_data[0])[1])
     for (thevalue) in dict_info.values():  #A way to get values from key/values pairs in dict
-        valueX = float(thevalue) #going to integer here was putting errors
-        value = int(valueX)
+        value = int(thevalue) 
     return value  
         
         
